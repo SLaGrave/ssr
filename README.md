4a38b1d27d32e848183df2e8bc8d69494853cc8b
Sam LaGrave <srlagrave@gmail.com>
2022-11-24 20:42:29 -0600

    Chapter 2 complete.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 My Rust personal project.
 
-It is an implementation of [Peter Shirley's](https://github.com/petershirley) *[Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html)*
+It is an implementation of [Peter Shirley's](https://github.com/petershirley) *[Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html)*.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+
+    // Image
+    const IMAGEWIDTH: i32 = 256;
+    const IMAGEHEIGHT: i32 = 256;
+
+    // Render
+    println!("P3");
+    println!("{} {}", IMAGEWIDTH, IMAGEHEIGHT);
+    println!("255");
+
+    for j in (0..IMAGEHEIGHT).rev() {
+        eprintln!("Scanlines remaining: {}", j);
+        for i in 0..IMAGEWIDTH {
+            let r: f32 = (i as f32)/((IMAGEWIDTH-1) as f32);
+            let g: f32 = (j as f32)/((IMAGEHEIGHT-1) as f32);
+            let b: f32 = 0.25;
+
+            let ir: i32 = (255.999 * r) as i32;
+            let ig: i32 = (255.999 * g) as i32;
+            let ib: i32 = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+    eprintln!("Done.");
 }
