97511e5768b3b316b0592c9b991377109836b0d7
Sam LaGrave <srlagrave@gmail.com>
2022-12-13 21:09:16 -0600

    Camera and Ray usage

--- src/ray.rs
+++ src/ray.rs
@@ -1,8 +1,8 @@
 use super::vec::{Vec3, Point3};
 
-struct Ray {
-    orig: Point3,
-    dir: Vec3
+pub struct Ray {
+    pub orig: Point3,
+    pub dir: Vec3
 }
 
 impl Ray {
@@ -17,11 +17,11 @@ impl Ray {
         self.orig
     }
 
-    pub direction(&self) -> Vec3 {
+    pub fn direction(&self) -> Vec3 {
         self.dir
     }
 
     pub fn at(&self, t: f64) -> Point3 {
-        self.orig + t * self.dir
+        self.orig + (self.dir * t)
     }
 }
